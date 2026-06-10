c7b2acfae0c639e3327b7e801fb239bb80eeba45
Mykhailo Shpeh <mykhailoshpeh@gmail.com>
2026-06-10 14:52:10 +0300

    lodash

--- src/components/AppSearchDebounce/AppSearchDebounce.jsx
+++ src/components/AppSearchDebounce/AppSearchDebounce.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import cards from "@/json/cards_10-10.json";
 import css from "./AppSearchDebounce.module.css";
+import debounce from "lodash.debounce";
 
 
 export class AppSearchDebounce extends Component {
@@ -9,18 +10,49 @@ export class AppSearchDebounce extends Component {
         inputValue: "",
         filteredArray: cards,
     }
-    
+
+    performSearch = textInput => {
+        // вся логіка фільтрації
+    }
+
+    debouncedSearch = debounce((text) => {
+        console.log("⏰debounce_text", text);
+        this.performSearch(text);
+    }, 500);
+
+
+
     handleChange = (event) => {
         console.log("Подія в input search: ");
         console.log("event: ", event)
         const inputData = event.target.value;
-        
-        
 
         this.setState({
             inputValue: inputData,
             filteredArray: cards.filter(item => item.title.toLowerCase().includes(inputData.trim().toLowerCase()))
         })
+    }
+
+    componentWillUnmount() {
+        this.debouncedSearch.cancel();
+    };
+
+
+    
+//     handleChange = debounce((event) => {
+//         console.log("Подія в input search: ");
+//         console.log("event: ", event)
+//         const inputData = event.target.value;
+
+//         this.setState({
+//             inputValue: inputData,
+//             filteredArray: cards.filter(item => item.title.toLowerCase().includes(inputData.trim().toLowerCase()))
+//         })
+// }, 500) //! не працює
+
+        
+
+        
         
 
         // console.log("filterArray: ", filterArray)
@@ -28,7 +60,7 @@ export class AppSearchDebounce extends Component {
         // const onlyInputSearchValue = this.state.aircraftsArrAfterFiltration.filter(item => item.name.brief.toLowerCase().startsWith(inputData.trim().toLowerCase()));
 
 
-    }
+    
 
     render() {
         
@@ -46,6 +78,8 @@ export class AppSearchDebounce extends Component {
                 type="text"
                 className={css.input}
                 onChange={this.handleChange}
+                // onChange={debounce(this.handleChange, 500)} //! не працює
+
             />
             <ul className={css.list}>
 
