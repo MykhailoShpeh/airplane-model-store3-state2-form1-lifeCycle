8e58cd7acb1c175ec339e6a071d3a9b2b30704d3
Mykhailo Shpeh <mykhailoshpeh@gmail.com>
2026-06-28 11:31:03 +0300

    filter for scale selection

--- src/components/ScaleSelection/ScaleSelection.jsx
+++ src/components/ScaleSelection/ScaleSelection.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import aircrafts from '@/json/aircrafts.json';
+
 import css from './ScaleSelection.module.css'
 
 const INITIAL_STATE = {
@@ -23,6 +25,14 @@ export class ScaleSelection extends Component {
         console.log("Значення name:", name);
         console.log("Значення value:", value);
 
+
+        const modelsSelectedScale = value == "all"
+            ? aircrafts
+            : aircrafts.filter(aircraft => aircraft.model.scale.some(item =>
+                item === Number(value)))
+
+        console.log("modelsSelectedScale: ", modelsSelectedScale);
+
         // //! Зберігаємо значення інпутів в state
         this.setState({
             //! Використовуємо властивості об'єкта, що обчислюються
@@ -41,22 +51,22 @@ export class ScaleSelection extends Component {
         return (
             <div className={css.scaleSelectionBox}>
                 <h3 className={css.scaleSelectionTitle}>Оберіть масштаб моделі:</h3>
-            <label>
-                <select
-                    className={css.scaleSelectionSelect}
-                    name="modelScale"
+                <label>
+                    <select
+                        className={css.scaleSelectionSelect}
+                        name="modelScale"
                         value={modelScale}
-                    onChange={this.handleChangeModelScale}
-                >
-                    <option className={css.scaleSelectionOption} value="all">Всі</option>
-                    <option className={css.scaleSelectionOption} value="200">1:200</option>
-                    <option className={css.scaleSelectionOption} value="144">1:144</option>
-                    <option className={css.scaleSelectionOption} value="100">1:100</option>
-                    <option className={css.scaleSelectionOption} value="72">1:72</option>
-                    <option className={css.scaleSelectionOption} value="60">1:60</option>
-                    <option className={css.scaleSelectionOption} value="48">1:48</option>
-                    <option className={css.scaleSelectionOption} value="32">1:32</option>
-                </select>
+                        onChange={this.handleChangeModelScale}
+                    >
+                        <option className={css.scaleSelectionOption} value="all">Всі</option>
+                        <option className={css.scaleSelectionOption} value="200">1:200</option>
+                        <option className={css.scaleSelectionOption} value="144">1:144</option>
+                        <option className={css.scaleSelectionOption} value="100">1:100</option>
+                        <option className={css.scaleSelectionOption} value="72">1:72</option>
+                        <option className={css.scaleSelectionOption} value="60">1:60</option>
+                        <option className={css.scaleSelectionOption} value="48">1:48</option>
+                        <option className={css.scaleSelectionOption} value="32">1:32</option>
+                    </select>
                 </label>
             </div>
         )
